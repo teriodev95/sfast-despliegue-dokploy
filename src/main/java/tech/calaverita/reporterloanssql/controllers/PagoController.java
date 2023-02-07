6e092af28453090c27303d9a783a82964ea5e1b2
Guillermo Austreberto Cordero Tapia <19307016@utcgg.edu.mx>
2023-02-07 16:52:03 -0600

    Se configuró un nuevo servicio para la creación de pagos

--- src/main/java/tech/calaverita/reporterloanssql/controllers/PagoController.java
+++ src/main/java/tech/calaverita/reporterloanssql/controllers/PagoController.java
@@ -2,12 +2,11 @@ package tech.calaverita.reporterloanssql.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import tech.calaverita.reporterloanssql.models.PagoModel;
+import tech.calaverita.reporterloanssql.models.PrestamoModel;
 import tech.calaverita.reporterloanssql.repositories.PagoRepository;
+import tech.calaverita.reporterloanssql.repositories.PrestamoRepository;
 
 import java.util.ArrayList;
 
@@ -16,6 +15,8 @@ import java.util.ArrayList;
 public class PagoController {
     @Autowired
     private PagoRepository pagoRepository;
+    @Autowired
+    private PrestamoRepository prestamoRepository;
 
     @GetMapping(path = "/dashboard-agencia/{agencia}/{anio}/{semana}")
     public @ResponseBody ArrayList<PagoModel> getPagosForDashboard(@PathVariable("agencia") String agencia, @PathVariable("anio") int anio, @PathVariable("semana") int semana){
@@ -41,5 +42,17 @@ public class PagoController {
         return pagoRepository.getPagoModelsByAnioAndSemana(anio, semana - 1);
     }
 
+    @PostMapping(path = "create-one")
+    public @ResponseBody String setPago(@RequestBody PagoModel pago){
 
+        PrestamoModel prestamo = prestamoRepository.getPrestamoModelsByPrestamoIdAndAgencia(pago.getPrestamoId(), pago.getAgente());
+
+        pago.setAbreCon(prestamo.getSaldo());
+        pago.setCierraCon(prestamo.getSaldo() - pago.getMonto());
+        pago.setEsPrimerPago(false);
+
+        pagoRepository.save(pago);
+
+        return "Pago Insertado con Éxito";
+    }
 }
