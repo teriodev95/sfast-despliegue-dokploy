f1f4580876dc6c75bd47dd5f3de288741b789a88
Guillermo Austreberto Cordero Tapia <gcordero4228@gmail.com>
2023-05-30 15:15:32 -0600

    Se agregó el CrossOrigin para desarrollo de pwa

--- src/main/java/tech/calaverita/reporterloanssql/ReporterLoansSqlApplication.java
+++ src/main/java/tech/calaverita/reporterloanssql/ReporterLoansSqlApplication.java
@@ -1,7 +1,11 @@
 package tech.calaverita.reporterloanssql;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ReporterLoansSqlApplication {
@@ -9,5 +13,4 @@ public class ReporterLoansSqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReporterLoansSqlApplication.class, args);
 	}
-
 }
--- src/main/java/tech/calaverita/reporterloanssql/controllers/PwaController.java
+++ src/main/java/tech/calaverita/reporterloanssql/controllers/PwaController.java
@@ -3,10 +3,7 @@ package tech.calaverita.reporterloanssql.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.calaverita.reporterloanssql.helpers.PwaUtil;
 import tech.calaverita.reporterloanssql.pojos.CobranzaPwa;
 import tech.calaverita.reporterloanssql.services.PrestamoService;
@@ -14,6 +11,7 @@ import tech.calaverita.reporterloanssql.services.XpressService;
 
 import java.util.ArrayList;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/xpress/v1/pwa")
 public class PwaController {
