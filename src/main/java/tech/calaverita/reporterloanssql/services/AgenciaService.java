8a02af1b89c91846d7e1f93f6721937a6f1f1065
Guillermo Austreberto Cordero Tapia <gcordero4228@gmail.com>
2023-05-31 23:19:14 -0600

    Se corrigieron los endpoint de agenciasGetByGerencia y gerenciasGetBySeguridad

--- src/main/java/tech/calaverita/reporterloanssql/services/AgenciaService.java
+++ src/main/java/tech/calaverita/reporterloanssql/services/AgenciaService.java
@@ -1,15 +1,19 @@
 package tech.calaverita.reporterloanssql.services;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.calaverita.reporterloanssql.repositories.AgenciaRepository;
 
 import java.util.ArrayList;
 
 @Service
-@AllArgsConstructor
 public class AgenciaService {
     private static AgenciaRepository agenciaRepository;
+
+    @Autowired
+    public AgenciaService(AgenciaRepository agenciaRepository) {
+        AgenciaService.agenciaRepository = agenciaRepository;
+    }
 
     public static ArrayList<String> getAgenciasByGerencia(String gerencia) {
         return agenciaRepository.getAgenciasByGerencia(gerencia);
--- src/main/java/tech/calaverita/reporterloanssql/services/GerenciaService.java
+++ src/main/java/tech/calaverita/reporterloanssql/services/GerenciaService.java
@@ -1,6 +1,7 @@
 package tech.calaverita.reporterloanssql.services;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.calaverita.reporterloanssql.repositories.GerenciaRepository;
 
@@ -10,6 +11,11 @@ import java.util.ArrayList;
 @AllArgsConstructor
 public class GerenciaService {
     private static GerenciaRepository gerenciaRepository;
+
+    @Autowired
+    public GerenciaService(GerenciaRepository gerenciaRepository) {
+        GerenciaService.gerenciaRepository = gerenciaRepository;
+    }
 
     public static ArrayList<String> getGerenciasBySeguridad(String seguridad) {
         return gerenciaRepository.getGerenciasBySeguridad(seguridad);
