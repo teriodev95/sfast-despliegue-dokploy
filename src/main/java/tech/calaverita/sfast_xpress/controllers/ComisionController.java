3c6cabd95bad93f49c9898825af6bbca60ff12cf
Guillermo Austreberto Cordero Tapia <gcordero4228@gmail.comclear>
2025-01-29 11:28:48 -0600

    Se calcula el bono y porcentaje de pago

--- src/main/java/tech/calaverita/sfast_xpress/controllers/ComisionController.java
+++ src/main/java/tech/calaverita/sfast_xpress/controllers/ComisionController.java
@@ -19,12 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tech.calaverita.sfast_xpress.models.mariaDB.AgenciaModel;
 import tech.calaverita.sfast_xpress.models.mariaDB.CalendarioModel;
+import tech.calaverita.sfast_xpress.models.mariaDB.CierreSemanalConsolidadoV2Model;
 import tech.calaverita.sfast_xpress.models.mariaDB.ComisionModel;
 import tech.calaverita.sfast_xpress.models.mariaDB.UsuarioModel;
 import tech.calaverita.sfast_xpress.models.mariaDB.VentaModel;
 import tech.calaverita.sfast_xpress.models.mariaDB.dynamic.PagoDynamicModel;
 import tech.calaverita.sfast_xpress.services.AgenciaService;
 import tech.calaverita.sfast_xpress.services.CalendarioService;
+import tech.calaverita.sfast_xpress.services.CierreSemanalConsolidadoV2Service;
 import tech.calaverita.sfast_xpress.services.ComisionService;
 import tech.calaverita.sfast_xpress.services.UsuarioService;
 import tech.calaverita.sfast_xpress.services.VentaService;
@@ -37,116 +39,192 @@ import tech.calaverita.sfast_xpress.utils.MyUtil;
 @RestController
 @RequestMapping(path = "/xpress/v1/comisiones")
 public class ComisionController {
-    private final CalendarioService calendarioService;
-    private final AgenciaService agenciaService;
-    private final PrestamoViewService prestamoViewService;
-    private final PagoDynamicService pagoDynamicService;
-    private final VentaService ventaService;
-    private final UsuarioService usuarioService;
-    private final ComisionService comisionService;
+        private final CalendarioService calendarioService;
+        private final AgenciaService agenciaService;
+        private final PrestamoViewService prestamoViewService;
+        private final PagoDynamicService pagoDynamicService;
+        private final VentaService ventaService;
+        private final UsuarioService usuarioService;
+        private final ComisionService comisionService;
+        private final CierreSemanalConsolidadoV2Service cierreSemanalConsolidadoV2Service;
 
-    public ComisionController(CalendarioService calendarioService, AgenciaService agenciaService,
-            PrestamoViewService prestamoViewService, PagoDynamicService pagoDynamicService, VentaService ventaService,
-            UsuarioService usuarioService, ComisionService comisionService) {
-        this.calendarioService = calendarioService;
-        this.agenciaService = agenciaService;
-        this.prestamoViewService = prestamoViewService;
-        this.pagoDynamicService = pagoDynamicService;
-        this.ventaService = ventaService;
-        this.usuarioService = usuarioService;
-        this.comisionService = comisionService;
-    }
+        public ComisionController(CalendarioService calendarioService, AgenciaService agenciaService,
+                        PrestamoViewService prestamoViewService, PagoDynamicService pagoDynamicService,
+                        VentaService ventaService, CierreSemanalConsolidadoV2Service cierreSemanalConsolidadoV2Service,
+                        UsuarioService usuarioService, ComisionService comisionService) {
+                this.calendarioService = calendarioService;
+                this.agenciaService = agenciaService;
+                this.prestamoViewService = prestamoViewService;
+                this.pagoDynamicService = pagoDynamicService;
+                this.ventaService = ventaService;
+                this.usuarioService = usuarioService;
+                this.comisionService = comisionService;
+                this.cierreSemanalConsolidadoV2Service = cierreSemanalConsolidadoV2Service;
+        }
 
-    @PostMapping(path = "/create-one")
-    public @ResponseBody ResponseEntity<ComisionModel> postCreateOne(@RequestBody ComisionModel comisionModel) {
-        ComisionModel responseComisionModel = this.comisionService.save(comisionModel);
-        HttpStatus responseHttpStatus = responseComisionModel != null ? HttpStatus.CREATED : HttpStatus.CONFLICT;
+        @PostMapping(path = "/create-one")
+        public @ResponseBody ResponseEntity<ComisionModel> postCreateOne(@RequestBody ComisionModel comisionModel) {
+                ComisionModel responseComisionModel = this.comisionService.save(comisionModel);
+                HttpStatus responseHttpStatus = responseComisionModel != null ? HttpStatus.CREATED
+                                : HttpStatus.CONFLICT;
 
-        return new ResponseEntity<>(responseComisionModel, responseHttpStatus);
-    }
+                return new ResponseEntity<>(responseComisionModel, responseHttpStatus);
+        }
 
-    @GetMapping(path = "/currentWeek/by_agencia/{agencia}")
-    public @ResponseBody ResponseEntity<ComisionModel> getCurrentWeek(@PathVariable String agencia) {
-        ComisionModel comisionModel = new ComisionModel();
+        @GetMapping(path = "/currentWeek/by_agencia/{agencia}")
+        public @ResponseBody ResponseEntity<ComisionModel> getCurrentWeek(@PathVariable String agencia) {
+                ComisionModel comisionModel = new ComisionModel();
 
-        CalendarioModel calendarioModel = this.calendarioService.findByFechaActual(LocalDate.now().toString());
+                CalendarioModel calendarioModel = this.calendarioService.findByFechaActual(LocalDate.now().toString());
 
-        // To easy code
-        int anio = calendarioModel.getAnio();
-        int semana = calendarioModel.getSemana();
-        String mes = LocalDate.now().getMonth().getDisplayName(TextStyle.FULL, new Locale("es", "MX"));
-        mes = mes.substring(0, 1).toUpperCase() + mes.substring(1);
-        String fechaJueves = LocalDate.parse(calendarioModel.getDesde()).plusDays(1) + " 23:59:59";
+                // To easy code
+                int anio = calendarioModel.getAnio();
+                int semana = calendarioModel.getSemana();
+                String mes = LocalDate.now().getMonth().getDisplayName(TextStyle.FULL, new Locale("es", "MX"));
+                mes = mes.substring(0, 1).toUpperCase() + mes.substring(1);
+                String fechaJueves = LocalDate.parse(calendarioModel.getDesde()).plusDays(1) + " 23:59:59";
 
-        CompletableFuture<AgenciaModel> agenciaModelCF = this.agenciaService.findByIdAsync(agencia);
-        CompletableFuture<UsuarioModel> agenteUsuarioModelCF = this.usuarioService
-                .findByAgenciaTipoAndStatusAsync(agencia, "Agente", true);
-        CompletableFuture<Double> debitoTotalCF = this.prestamoViewService
-                .findDebitoTotalByAgenciaSaldoAlIniciarSemanaGreaterThanAndNotAnioAndSemana(agencia, 0D, anio, semana);
-        CompletableFuture<Integer> clientesPagoCompletoCF = this.pagoDynamicService
-                .findClientesPagoCompletoByAgenciaAnioAndSemanaAsync(agencia, anio, semana);
-        CompletableFuture<Double> cobranzaPuraHastaJuevesCF = this.pagoDynamicService
-                .findCobranzaPuraByAgenciaAnioSemanaAndFechaPagoLessThanEqual(agencia, anio, semana, fechaJueves);
-        CompletableFuture<ArrayList<PagoDynamicModel>> pagoDynamicModelsCF = this.pagoDynamicService
-                .findByAgenciaAnioSemanaAndEsPrimerPago(agencia, anio, semana, false);
-        CompletableFuture<ArrayList<VentaModel>> ventaModelsCF = this.ventaService.findByAgenciaAnioAndSemanaAsync(
-                agencia,
-                anio, semana);
+                CompletableFuture<AgenciaModel> agenciaModelCF = this.agenciaService.findByIdAsync(agencia);
+                CompletableFuture<UsuarioModel> agenteUsuarioModelCF = this.usuarioService
+                                .findByAgenciaTipoAndStatusAsync(agencia, "Agente", true);
+                CompletableFuture<Double> debitoTotalCF = this.prestamoViewService
+                                .findDebitoTotalByAgenciaSaldoAlIniciarSemanaGreaterThanAndNotAnioAndSemana(agencia, 0D,
+                                                anio, semana);
+                CompletableFuture<Integer> clientesPagoCompletoCF = this.pagoDynamicService
+                                .findClientesPagoCompletoByAgenciaAnioAndSemanaAsync(agencia, anio, semana);
+                CompletableFuture<Double> cobranzaPuraHastaJuevesCF = this.pagoDynamicService
+                                .findCobranzaPuraByAgenciaAnioSemanaAndFechaPagoLessThanEqual(agencia, anio, semana,
+                                                fechaJueves);
+                CompletableFuture<ArrayList<PagoDynamicModel>> pagoDynamicModelsCF = this.pagoDynamicService
+                                .findByAgenciaAnioSemanaAndEsPrimerPago(agencia, anio, semana, false);
+                CompletableFuture<ArrayList<VentaModel>> ventaModelsCF = this.ventaService
+                                .findByAgenciaAnioAndSemanaAsync(
+                                                agencia,
+                                                anio, semana);
 
-        ArrayList<PagoDynamicModel> pagoDynamicModels = pagoDynamicModelsCF.join();
+                ArrayList<PagoDynamicModel> pagoDynamicModels = pagoDynamicModelsCF.join();
 
-        double cobranzaPura = 0;
-        double montoExcedente = 0;
-        double cobranzaTotal = 0;
-        for (PagoDynamicModel pagoDynamicModel : pagoDynamicModels) {
-            if (pagoDynamicModel.getMonto() < pagoDynamicModel.getTarifa()) {
-                cobranzaPura += pagoDynamicModel.getMonto();
-                cobranzaTotal += pagoDynamicModel.getMonto();
-            } else {
-                cobranzaPura += pagoDynamicModel.getTarifa();
-                cobranzaTotal += pagoDynamicModel.getMonto();
+                double cobranzaPura = 0;
+                double montoExcedente = 0;
+                double cobranzaTotal = 0;
+                for (PagoDynamicModel pagoDynamicModel : pagoDynamicModels) {
+                        if (pagoDynamicModel.getMonto() < pagoDynamicModel.getTarifa()) {
+                                cobranzaPura += pagoDynamicModel.getMonto();
+                                cobranzaTotal += pagoDynamicModel.getMonto();
+                        } else {
+                                cobranzaPura += pagoDynamicModel.getTarifa();
+                                cobranzaTotal += pagoDynamicModel.getMonto();
 
-                if (!pagoDynamicModel.getTipo().equals("Liquidacion")) {
-                    montoExcedente += pagoDynamicModel.getMonto() - pagoDynamicModel.getTarifa();
+                                if (!pagoDynamicModel.getTipo().equals("Liquidacion")) {
+                                        montoExcedente += pagoDynamicModel.getMonto() - pagoDynamicModel.getTarifa();
+                                }
+                        }
                 }
-            }
+
+                ArrayList<VentaModel> ventaModels = ventaModelsCF.join();
+
+                double primerosPagos = 0;
+                for (VentaModel ventaModel : ventaModels) {
+                        primerosPagos += ventaModel.getPrimerPago();
+                }
+
+                AgenciaModel agenciaModel = agenciaModelCF.join();
+                UsuarioModel agenteUsuarioModel = agenteUsuarioModelCF.join();
+                Double debitoTotal = debitoTotalCF.join();
+                Integer clientesPagoCompleto = clientesPagoCompletoCF.join();
+                Double cobranzaPuraHastaJueves = cobranzaPuraHastaJuevesCF.join();
+
+                comisionModel.setSemana(semana);
+                comisionModel.setMes(mes);
+                comisionModel.setAnio(anio);
+                comisionModel.setAgencia(agencia);
+                comisionModel.setGerencia(agenciaModel.getGerenciaId());
+                comisionModel.setDebito(MyUtil.getDouble(debitoTotal));
+                comisionModel.setNivel(
+                                BalanceAgenciaUtil.getNivelAgente(clientesPagoCompleto, cobranzaPuraHastaJueves,
+                                                agenteUsuarioModel));
+                comisionModel.setCobranzaPura(MyUtil.getDouble(cobranzaPura));
+                comisionModel.setRendimiento(MyUtil.getDouble(cobranzaPura / debitoTotal * 100));
+                comisionModel.setMontoExcedente(MyUtil.getDouble(montoExcedente));
+                comisionModel.setCobranzaTotal(MyUtil.getDouble(cobranzaTotal));
+                comisionModel.setPrimerosPagos(MyUtil.getDouble(primerosPagos));
+                comisionModel.setPorcentajeComisionCobranza(
+                                BalanceAgenciaUtil.getPorcentajeComisionCobranza(comisionModel.getNivel()));
+                comisionModel.setPorcentajeBonoMensual(0);
+                comisionModel
+                                .setComisionCobranza(MyUtil.getDouble(
+                                                (cobranzaTotal + primerosPagos) / 100
+                                                                * comisionModel.getPorcentajeComisionCobranza()));
+                comisionModel.setComisionVentas(ventaModels.size() * 100D);
+                comisionModel.setBonos(0D);
+
+                calcularPagoDeBonos(comisionModel, calendarioModel, agenteUsuarioModel);
+
+                return new ResponseEntity<>(comisionModel, HttpStatus.OK);
         }
 
-        ArrayList<VentaModel> ventaModels = ventaModelsCF.join();
+        public void calcularPagoDeBonos(ComisionModel comisionModel,
+                        CalendarioModel semanaActualCalendarioModel,
+                        UsuarioModel usuarioModel) {
+                if (semanaActualCalendarioModel.isPagoBono()) {
+                        String[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+                                        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+                        int indiceMes = 0;
+                        for (int i = 0; i < meses.length; i++) {
+                                if (semanaActualCalendarioModel.getMes().equals(meses[i])) {
+                                        indiceMes = i - 1;
+                                        break;
+                                }
+                        }
 
-        double primerosPagos = 0;
-        for (VentaModel ventaModel : ventaModels) {
-            primerosPagos += ventaModel.getPrimerPago();
+                        // To easy code
+                        int anio = semanaActualCalendarioModel.getAnio();
+
+                        ArrayList<CalendarioModel> semanasDelMes = this.calendarioService
+                                        .findByAnioAndMesAsync(indiceMes < 0 ? anio - 1 : anio,
+                                                        meses[indiceMes < 0 ? 11 : indiceMes])
+                                        .join();
+
+                        CierreSemanalConsolidadoV2Model cierreSemanalConsolidadoV2Model;
+                        Double cobranzaTotal = 0D;
+                        Double rendimiento = 0D;
+                        Integer clientesPagoCompleto = 0;
+                        for (int i = 0; i < semanasDelMes.size(); i++) {
+                                cierreSemanalConsolidadoV2Model = this.cierreSemanalConsolidadoV2Service
+                                                .findByAgenciaAnioAndSemana(comisionModel.getAgencia(),
+                                                                semanasDelMes.get(i).getAnio(),
+                                                                semanasDelMes.get(i).getSemana());
+                                ComisionModel anteriorComisionModel = this.comisionService.findByAgenciaAnioAndSemana(
+                                                comisionModel.getAgencia(), semanasDelMes.get(i).getAnio(),
+                                                semanasDelMes.get(i).getSemana());
+
+                                if (cierreSemanalConsolidadoV2Model != null && anteriorComisionModel != null) {
+                                        cobranzaTotal += anteriorComisionModel.getCobranzaTotal()
+                                                        + anteriorComisionModel.getPrimerosPagos()
+                                                        - cierreSemanalConsolidadoV2Model.getLiquidaciones();
+
+                                        rendimiento += anteriorComisionModel.getRendimiento();
+
+                                        clientesPagoCompleto += cierreSemanalConsolidadoV2Model.getClientes()
+                                                        - cierreSemanalConsolidadoV2Model.getPagosReducidos()
+                                                        - cierreSemanalConsolidadoV2Model.getNoPagos();
+                                }
+                        }
+
+                        rendimiento = rendimiento / semanasDelMes.size();
+                        clientesPagoCompleto = clientesPagoCompleto / semanasDelMes.size();
+                        cobranzaTotal = MyUtil.getDouble(cobranzaTotal);
+
+                        System.out.println("cobranzaTotal: " + cobranzaTotal);
+                        System.out.println("rendimiento: " + rendimiento);
+                        System.out.println("clientesPagoCompleto: " + clientesPagoCompleto);
+
+                        comisionModel.setPromedioClientesPagoCompleto(clientesPagoCompleto);
+                        comisionModel.setPorcentajeBonoMensual(
+                                        BalanceAgenciaUtil.getPorcentajeBonoMensual(clientesPagoCompleto, rendimiento,
+                                                        usuarioModel));
+                        comisionModel.setBonos(MyUtil.getDouble(
+                                        cobranzaTotal / 100 * comisionModel.getPorcentajeBonoMensual()));
+                }
         }
-
-        AgenciaModel agenciaModel = agenciaModelCF.join();
-        UsuarioModel agenteUsuarioModel = agenteUsuarioModelCF.join();
-        Double debitoTotal = debitoTotalCF.join();
-        Integer clientesPagoCompleto = clientesPagoCompletoCF.join();
-        Double cobranzaPuraHastaJueves = cobranzaPuraHastaJuevesCF.join();
-
-        comisionModel.setSemana(semana);
-        comisionModel.setMes(mes);
-        comisionModel.setAnio(anio);
-        comisionModel.setAgencia(agencia);
-        comisionModel.setGerencia(agenciaModel.getGerenciaId());
-        comisionModel.setDebito(MyUtil.getDouble(debitoTotal));
-        comisionModel.setNivel(
-                BalanceAgenciaUtil.getNivelAgente(clientesPagoCompleto, cobranzaPuraHastaJueves, agenteUsuarioModel));
-        comisionModel.setCobranzaPura(MyUtil.getDouble(cobranzaPura));
-        comisionModel.setRendimiento(MyUtil.getDouble(cobranzaPura / debitoTotal * 100));
-        comisionModel.setMontoExcedente(MyUtil.getDouble(montoExcedente));
-        comisionModel.setCobranzaTotal(MyUtil.getDouble(cobranzaTotal));
-        comisionModel.setPrimerosPagos(MyUtil.getDouble(primerosPagos));
-        comisionModel.setPorcentajeComisionCobranza(
-                BalanceAgenciaUtil.getPorcentajeComisionCobranza(comisionModel.getNivel()));
-        comisionModel.setPorcentajeBonoMensual(0);
-        comisionModel
-                .setComisionCobranza(MyUtil.getDouble(
-                        (cobranzaTotal + primerosPagos) / 100 * comisionModel.getPorcentajeComisionCobranza()));
-        comisionModel.setComisionVentas(ventaModels.size() * 100D);
-        comisionModel.setBonos(0D);
-
-        return new ResponseEntity<>(comisionModel, HttpStatus.OK);
-    }
 }
--- src/main/java/tech/calaverita/sfast_xpress/models/mariaDB/ComisionModel.java
+++ src/main/java/tech/calaverita/sfast_xpress/models/mariaDB/ComisionModel.java
@@ -1,5 +1,7 @@
 package tech.calaverita.sfast_xpress.models.mariaDB;
 
+import java.beans.Transient;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import jakarta.persistence.Entity;
@@ -34,4 +36,6 @@ public class ComisionModel {
     private Double comisionCobranza;
     private Double comisionVentas;
     private Double bonos;
+    @jakarta.persistence.Transient
+    private Integer promedioClientesPagoCompleto;
 }
