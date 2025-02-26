6a99d03040e95e59663e27b180ee2da5d13a0dbc
Guillermo Austreberto Cordero Tapia <gcordero4228@gmail.comclear>
2025-02-26 15:07:29 -0600

    Se agregaron constructores para manejar mejor la semana anterior

--- src/main/java/tech/calaverita/sfast_xpress/models/mariaDB/CalendarioModel.java
+++ src/main/java/tech/calaverita/sfast_xpress/models/mariaDB/CalendarioModel.java
@@ -1,6 +1,10 @@
 package tech.calaverita.sfast_xpress.models.mariaDB;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
@@ -15,4 +19,23 @@ public class CalendarioModel {
     private String desde;
     private String hasta;
     private boolean pagoBono;
+
+    public CalendarioModel() {
+        this.semana = 0;
+        this.anio = 0;
+        this.mes = "";
+        this.desde = "";
+        this.hasta = "";
+        this.pagoBono = false;
+    }
+
+    public CalendarioModel(CalendarioModel calendarioModel) {
+        this();
+        this.semana = calendarioModel.getSemana();
+        this.anio = calendarioModel.getAnio();
+        this.mes = calendarioModel.getMes();
+        this.desde = calendarioModel.getDesde();
+        this.hasta = calendarioModel.getHasta();
+        this.pagoBono = calendarioModel.isPagoBono();
+    }
 }
