837a63e9d6d40ef5ae2b213c6fd4c741fd010253
Guillermo Austreberto Cordero Tapia <gcordero4228@gmail.com>
2025-03-07 23:43:18 -0600

    Se quitó el fetch type lazy de gerencias

--- src/main/java/tech/calaverita/sfast_xpress/models/mariaDB/UsuarioModel.java
+++ src/main/java/tech/calaverita/sfast_xpress/models/mariaDB/UsuarioModel.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -42,7 +41,7 @@ public class UsuarioModel {
     // @OneToMany(mappedBy = "usuarioModel")
     // private List<GastoModel> gastoModels;
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "gerencia", insertable = false, updatable = false)
     private GerenciaModel gerenciaModel;
     @JsonIgnore
