be15516c717235f52dc8e6e9c0c264cc3362802d
Diego Rico <diegoricosanchez54@gmail.com>
2025-03-14 15:47:27 -0600

    Añadidos endpoints con filtros a solicitudes

--- src/main/java/tech/calaverita/sfast_xpress/controllers/PGS/SolicitudController.java
+++ src/main/java/tech/calaverita/sfast_xpress/controllers/PGS/SolicitudController.java
@@ -10,7 +10,10 @@ import tech.calaverita.sfast_xpress.mappers.SolicitudMapper;
 import tech.calaverita.sfast_xpress.models.mariaDB.SolicitudModel;
 import tech.calaverita.sfast_xpress.services.SolicitudService;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Map;
+
 
 @CrossOrigin
 @RestController
@@ -99,4 +102,78 @@ public final class SolicitudController {
 
         return new ResponseEntity<>(response, httpStatus);
     }
+
+    @GetMapping("/{agencia}/{anio}/{semana}")
+    public ResponseEntity<Object> getSolicitudesByAgenciaAnioY3Semanas(
+        @PathVariable String agencia,
+        @PathVariable Integer anio,
+        @PathVariable Integer semana,
+        @RequestParam String status ) {
+            
+        ArrayList<SolicitudDTO> solicitudDTOS = new ArrayList<>();
+        HttpStatus httpStatus = HttpStatus.OK;
+        Integer semanaAnterior = semana - 2;
+        try {
+            if (semana < 3) {
+                semanaAnterior = 1;
+            }
+            if (status == null) {
+                status = "";
+            }
+            solicitudDTOS = new SolicitudMapper().mapOuts(this.solicitudService.findByAgenciaAnioYEntre3Semanas(agencia, anio, semana, semanaAnterior, status));
+            
+    
+            if (httpStatus.equals(HttpStatus.OK) && solicitudDTOS.isEmpty()) {
+                httpStatus = HttpStatus.NO_CONTENT;
+            }
+    
+            return new ResponseEntity<>(solicitudDTOS, httpStatus);
+
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(Map.of(
+                    "timestamp", LocalDateTime.now(),
+                    "status", httpStatus,
+                    "error", "Internal Server Error",
+                    "message", e.getMessage()));
+        }
+
+        
+    }
+
+    @GetMapping("/status/{agencia}/{anio}/{semana}")
+    public ResponseEntity<Object> getSolicitudesByAgenciaAnioSemanaYStatus(
+        @PathVariable String agencia,
+        @PathVariable Integer anio,
+        @PathVariable Integer semana,
+        @RequestParam String status ) {
+            
+        ArrayList<SolicitudDTO> solicitudDTOS = new ArrayList<>();
+        HttpStatus httpStatus = HttpStatus.OK;
+        try {
+            if (status == null) {
+                status = "";
+            }
+            solicitudDTOS = new SolicitudMapper().mapOuts(this.solicitudService.findByAgenciaAnioSemanasYStatus(agencia, anio, semana, status));
+            
+    
+            if (httpStatus.equals(HttpStatus.OK) && solicitudDTOS.isEmpty()) {
+                httpStatus = HttpStatus.NO_CONTENT;
+            }
+    
+            return new ResponseEntity<>(solicitudDTOS, httpStatus);
+
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(Map.of(
+                    "timestamp", LocalDateTime.now(),
+                    "status", httpStatus,
+                    "error", "Internal Server Error",
+                    "message", e.getMessage()));
+        }
+
+        
+    }
+    
+    
 }
--- src/main/java/tech/calaverita/sfast_xpress/repositories/SolicitudRepository.java
+++ src/main/java/tech/calaverita/sfast_xpress/repositories/SolicitudRepository.java
@@ -1,5 +1,6 @@
 package tech.calaverita.sfast_xpress.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import tech.calaverita.sfast_xpress.models.mariaDB.SolicitudModel;
@@ -11,4 +12,10 @@ public interface SolicitudRepository extends CrudRepository<SolicitudModel, Stri
     ArrayList<SolicitudModel> findByGerencia(String gerencia);
 
     ArrayList<SolicitudModel> findByAgencia(String agencia);
+
+    @Query("SELECT s FROM SolicitudModel s WHERE s.agencia = :agencia AND s.anio = :anio AND s.semana BETWEEN :semanaAnterior AND :semanaActual AND s.status LIKE %:status%")
+    ArrayList<SolicitudModel> findByAgenciaAnioStatusYEntre3Semanas(String agencia, Integer anio, Integer semanaActual, Integer semanaAnterior, String status);
+
+    @Query("SELECT s FROM SolicitudModel s WHERE s.agencia = :agencia AND s.anio = :anio AND s.semana = :semana AND s.status LIKE %:status%")
+    ArrayList<SolicitudModel> findByAgenciaAnioSemanasYStatus(String agencia, Integer anio, Integer semana, String status);
 }
