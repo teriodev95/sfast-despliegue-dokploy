b8447abaf165488b9e1d128b4a11c48d33edd8e0
Guillermo Austreberto Cordero Tapia <gcordero4228@gmail.com>
2025-03-24 14:25:04 -0600

    Se agregó el endpoint que filtra por usuarioId, año y semana y se renombró el de gerencia, año y semana

--- src/main/java/tech/calaverita/sfast_xpress/controllers/PGS/IncidenteReposicionController.java
+++ src/main/java/tech/calaverita/sfast_xpress/controllers/PGS/IncidenteReposicionController.java
@@ -50,7 +50,7 @@ public class IncidenteReposicionController {
         return new ResponseEntity<>(incidentes, httpStatus);
     }
 
-    @GetMapping("/by_gerencia_anio_and_semana/{gerencia}/{anio}/{semana}")
+    @GetMapping("/gerencia/{gerencia}/anio/{anio}/semana/{semana}")
     public ResponseEntity<List<IncidenteReposicionModel>> filterByGerenciaAnioSemana(
             @PathVariable String gerencia,
             @PathVariable int anio,
@@ -66,43 +66,56 @@ public class IncidenteReposicionController {
         }
     }
 
+    @GetMapping("/usuarioId/{usuarioId}/anio/{anio}/semana/{semana}")
+    public ResponseEntity<List<IncidenteReposicionModel>> filterByGerenciaAnioSemana(
+            @PathVariable int usuarioId,
+            @PathVariable int anio,
+            @PathVariable int semana) {
+
+        try {
+            List<IncidenteReposicionModel> resultados = this.incidenteReposicionService
+                    .findByUsuarioIdAnioSemanaAsync(usuarioId, anio, semana)
+                    .get();
+            return ResponseEntity.ok(resultados);
+        } catch (InterruptedException | ExecutionException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
     @PostMapping("/create-one")
     public ResponseEntity<Object> create(@RequestBody IncidenteReposicionModel incidente) {
         try {
             // Validación simple (puedes agregar más con @Valid)
-            if (incidente.getCategoria() == null || incidente.getCategoria().length() == 0 
-            || incidente.getTipo() == null || incidente.getTipo().length() == 0
-            || incidente.getMonto() == null) {
+            if (incidente.getCategoria() == null || incidente.getCategoria().length() == 0
+                    || incidente.getTipo() == null || incidente.getTipo().length() == 0
+                    || incidente.getMonto() == null) {
                 return ResponseEntity.badRequest().body(Map.of(
-                    "timestamp", LocalDateTime.now(),
-                    "status", 400,
-                    "error", "Bad Request",
-                    "message", "Los campos 'categoria', 'tipo' y 'monto' son obligatorios"
-                ));
+                        "timestamp", LocalDateTime.now(),
+                        "status", 400,
+                        "error", "Bad Request",
+                        "message", "Los campos 'categoria', 'tipo' y 'monto' son obligatorios"));
             }
 
             if (incidente.getMonto() == 0) {
                 return ResponseEntity.badRequest().body(Map.of(
-                    "timestamp", LocalDateTime.now(),
-                    "status", 400,
-                    "error", "Bad Request",
-                    "message", "El monto no puede ser 0"
-                ));
+                        "timestamp", LocalDateTime.now(),
+                        "status", 400,
+                        "error", "Bad Request",
+                        "message", "El monto no puede ser 0"));
             }
 
             // incidente.setFecha(LocalDate.now(ZoneId.of("America/Mexico_City")));
-    
+
             IncidenteReposicionModel savedIncidente = incidenteReposicionRepository.save(incidente);
             return ResponseEntity.ok(savedIncidente);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(Map.of(
-                "timestamp", LocalDateTime.now(),
-                "status", 500,
-                "error", "Internal Server Error",
-                "message", "Error al guardar el incidente: " + e.getMessage()
-            ));
+                    "timestamp", LocalDateTime.now(),
+                    "status", 500,
+                    "error", "Internal Server Error",
+                    "message", "Error al guardar el incidente: " + e.getMessage()));
         }
-        
+
     }
 
 }
